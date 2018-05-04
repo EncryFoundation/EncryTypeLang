fc3d001b5dfb82560212075d8996acdc7a26ccad
Ilya Oskin <ilya.arcadich@gmail.com>
2018-05-04 21:12:37 +0300

    AnyCodec improved.

--- src/main/scala/encrytl/core/codec/AnyCodec.scala
+++ src/main/scala/encrytl/core/codec/AnyCodec.scala
@@ -5,36 +5,48 @@ import java.nio.charset.Charset
 import com.google.common.primitives.{Ints, Longs}
 import encrytl.core.{TypedObject, TypedObjectCodec, Types}
 import scodec.bits.BitVector
-import scodec.{Attempt, DecodeResult}
+
+import scala.util.Try
 
 object AnyCodec {
 
   import scodec.codecs._
 
-  def encode(vl: Any): Array[Byte] = vl match {
+  def encode(v: Any): Array[Byte] = v match {
     case int: Types.EInt.Underlying => Ints.toByteArray(int)
     case long: Types.ELong.Underlying => Longs.toByteArray(long)
     case bool: Types.EBoolean.Underlying => if (bool) Array(1) else Array(0)
-    case str: Types.EString.Underlying =>
-      val bytes = str.getBytes(Charset.defaultCharset)
-      uint16.encode(bytes.length).require.toByteArray ++ bytes
-    case bytes: Types.EByteVector.Underlying => uint16.encode(bytes.length).require.toByteArray ++ bytes
+    case str: Types.EString.Underlying => str.getBytes(Charset.defaultCharset)
+    case bytes: Types.EByteVector.Underlying => bytes
     case obj: TypedObject => TypedObjectCodec.encode(obj)
-    case intList: List[Int] => intList.size.toByte +: intList.map(encode).reduce(_ ++ _)
-    case longList: List[Long] => longList.size.toByte +: longList.map(encode).reduce(_ ++ _)
-    case boolList: List[Boolean] => boolList.size.toByte +: boolList.map(encode).reduce(_ ++ _)
-    case list: List[_] => list.map { el =>
+    case list: List[_] => uint8.encode(list.size).require.toByteArray ++ list.map { el =>
       val elBytes = encode(el)
       uint16.encode(elBytes.length).require.toByteArray ++ elBytes
     }.reduce(_ ++ _)
-    case dict: Map[_, _] => dict.map { case (k, v) =>
+    case dict: Map[_, _] => dict.map { case (k, vl) =>
       val kBytes = encode(k)
-      val vBytes = encode(v)
+      val vBytes = encode(vl)
       kBytes.length.toByte +: (kBytes ++ uint16.encode(vBytes.length).require.toByteArray ++ vBytes)
     }.reduce(_ ++ _)
     case opt: Option[_] => if (opt.isEmpty) Array(0) else 1.toByte +: opt.map(encode).get
     case _ => throw new Exception("Unsupported type")
   }
 
-  def decode[T](b: BitVector): Attempt[DecodeResult[T]] = ???
+  def decode(tpe: Types.EType, bytes: Array[Byte]): Try[tpe.Underlying] = Try {
+    (tpe match {
+      case _: Types.EInt.type => Ints.fromByteArray(bytes)
+      case _: Types.ELong.type => Longs.fromByteArray(bytes)
+      case _: Types.EBoolean.type => if (bytes.head == 1) true else false
+      case _: Types.EString.type => new String(bytes, Charset.defaultCharset)
+      case _: Types.EByteVector.type => bytes
+      case _: Types.EProduct => TypedObjectCodec.decode(bytes).get
+      case Types.EList(inT) =>
+        val eltsQty = uint8.decode(BitVector(bytes.head)).require.value
+        (0 until eltsQty).foldLeft(bytes.tail, List.empty[inT.Underlying]) { case ((leftBytes, acc), _) =>
+          val len = uint16.decode(BitVector(leftBytes.take(2))).require.value
+          leftBytes.drop(2 + len) -> (acc :+ decode(inT, leftBytes.slice(2, len + 2)).get.asInstanceOf[inT.Underlying])
+        }._2
+      // TODO: Dict and Option
+    }).asInstanceOf[tpe.Underlying]
+  }
 }
--- src/test/scala/encrytl/core/codec/AnyCodecTest.scala
+++ src/test/scala/encrytl/core/codec/AnyCodecTest.scala
@@ -1,25 +1,41 @@
 package encrytl.core.codec
 
+import encrytl.core.Types
 import org.scalatest.{Matchers, PropSpec}
-
-import scala.util.Try
 
 class AnyCodecTest extends PropSpec with Matchers {
 
-  property("encode") {
+  property("Primitives encoding/decoding") {
 
     val str = "some_string"
     val int = 1234
     val bool = true
-    val strList = List("some", "string", "int", "long")
-    val intList = List(1, 2, 3, 4, 5, 6)
 
-    Try {
-      AnyCodec.encode(str)
-      AnyCodec.encode(int)
-      AnyCodec.encode(bool)
-      AnyCodec.encode(strList)
-      AnyCodec.encode(intList)
-    }.isSuccess shouldBe true
+    val strEnc = AnyCodec.encode(str)
+    val intEnc = AnyCodec.encode(int)
+    val boolEnc = AnyCodec.encode(bool)
+
+    val strDec = AnyCodec.decode(Types.EString, strEnc)
+    val intDec = AnyCodec.decode(Types.EInt, intEnc)
+    val boolDec = AnyCodec.decode(Types.EBoolean, boolEnc)
+
+    Seq(strDec, intDec, boolDec).forall(_.isSuccess) shouldBe true
+
+    strDec.get == str shouldBe true
+    intDec.get == int shouldBe true
+    boolDec.get == bool shouldBe true
+  }
+
+  property("List encoding/decoding") {
+
+    val strList: List[String] = List("some", "string", "int", "long")
+
+    val strListEnc = AnyCodec.encode(strList)
+
+    val strListDec = AnyCodec.decode(Types.EList(Types.EString), strListEnc)
+
+    strListDec.isSuccess shouldBe true
+
+    strListDec.get sameElements strList shouldBe true
   }
 }
